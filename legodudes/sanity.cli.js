a4a9092bc536e11fa9ba725b722f0f7ca48b1e6d
Sondre Jønsson <sondrej@hiof.no>
2024-03-21 09:53:21 +0100

    Laget diverse fra forelesning.

---
+++ legodudes/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'goso967h',
+    dataset: 'production'
+  }
+})
---
+++ legodudes/schemaTypes/index.js
@@ -0,0 +1,4 @@
+import { categories } from "./categories";
+import { products } from "./product";
+
+export const schemaTypes = [categories, products]
